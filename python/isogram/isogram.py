f9e86cb7f742d13256a6bea1b51d6f9d9f4b19a3
dbjennings <dbjennings@gmail.com>
2021-02-27 10:38:32 -0600

    isogram.py complete

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,17 @@
-def is_isogram(string):
-    pass
+def is_isogram(string: str) -> bool:
+    """
+    Function: Determines if a word or phrase is an isogram.
+    Note: In this case, spaces/hyphens can appear multiple times
+
+         Parameters:
+            string (str): The word/phrase being tested
+
+        Returns:
+            bool: True if string is an isogram
+
+    Definitions:
+        isogram - a word or phrase that has no repeating letters
+    """
+    
+    stripped = string.lower().replace('-','').replace(' ','')
+    return len(set(stripped)) == len(stripped)
